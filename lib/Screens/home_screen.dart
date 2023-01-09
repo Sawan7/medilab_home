dd73758d702ffd7dd73600b90387353872141fe8
Sawan bhardwaj <sawanbhardwaj9268@gmail.com>
2023-01-09 20:37:00 +0530

    initial commit

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,300 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            backgroundColor: Colors.blue,
+            body: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: const [
+                          CircleAvatar(
+                              radius: 25,
+                              child: ClipOval(
+                                child: Image(
+                                  image: NetworkImage(
+                                      'https://kalingatv.com/wp-content/uploads/2017/04/Virat-Kohli-RCB.jpg'),
+                                ),
+                              )),
+                          SizedBox(width: 5),
+                          Text(
+                            'Virat Kohli',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.white.withOpacity(0.3),
+                                  spreadRadius: 5,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 2))
+                            ],
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0)),
+                        height: 170,
+                        width: double.infinity,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 35,
+                                    width: 35,
+                                    decoration: const BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'images/ml_dashboardClinicVisit.png'))),
+                                  ),
+                                  const Text(
+                                    'Appointment',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  const SizedBox(
+                                    height: 22,
+                                  ),
+                                  Container(
+                                    height: 35,
+                                    width: 35,
+                                    decoration: const BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'images/ml_covidSpecialist.png'))),
+                                  ),
+                                  const Text(
+                                    'Covid-19',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 22,
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 35,
+                                    width: 35,
+                                    decoration: const BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'images/ml_dashboardHomeVisit.png'))),
+                                  ),
+                                  const Text(
+                                    'Home Visit',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  const SizedBox(height: 20),
+                                  Container(
+                                    height: 35,
+                                    width: 35,
+                                    decoration: const BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'images/ml_dashboardPharmacy.png'))),
+                                  ),
+                                  const Text(
+                                    'Pharmacy',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 22,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 35,
+                                    width: 35,
+                                    decoration: const BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'images/ml_eyeSpecialist.png'))),
+                                  ),
+                                  const Text(
+                                    'Eye Consult',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  const SizedBox(height: 22),
+                                  Container(
+                                    height: 35,
+                                    width: 35,
+                                    decoration: const BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'images/ml_bloodTest.png'))),
+                                  ),
+                                  const Text(
+                                    'Blood Donate',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Expanded(
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20)),
+                            color: Colors.white,
+                          ),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: const [
+                                    Text(
+                                      'Top Hospitals',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Expanded(
+                                        child: SizedBox(
+                                      width: 10,
+                                    )),
+                                    Text(
+                                      'View all >',
+                                      style: TextStyle(
+                                          color: Colors.blue,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        height: 180,
+                                          width: 300,
+                                          decoration: const BoxDecoration(
+                                            borderRadius: BorderRadius.only(
+                                                topLeft: Radius.circular(20),
+                                                topRight: Radius.circular(20)),
+                                            color: Colors.white,
+                                          ),
+                                          child: const Card(
+                                              child: Image(
+                                                  image: AssetImage(
+                                                      'images/ml_tophospitalone.jpg')))),
+                                      Container(
+                                          height: 180,
+                                          width: 300,
+                                          child: const Card(
+                                              child: Image(
+                                                  image: AssetImage(
+                                                      'images/ml_tophospitalone.jpg')))),
+                                      Container(
+                                          height: 180,
+                                          width: 300,
+                                          child: const Card(
+                                              child: Image(
+                                                  image: AssetImage(
+                                                      'images/ml_tophospitalone.jpg')))),
+                                      Container(
+                                          height: 180,
+                                          width: 300,
+                                          child: const Card(
+                                              child: Image(
+                                                  image: AssetImage(
+                                                      'images/ml_tophospitalone.jpg'))))
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ]))));
+  }
+}
+
+// Expanded(
+//   child: Container(
+//     decoration: BoxDecoration(
+//         boxShadow: [
+//           BoxShadow(
+//               color: Colors.white.withOpacity(0.3),
+//               spreadRadius: 5,
+//               blurRadius: 5,
+//               offset: const Offset(0, 2))
+//         ],
+//         color: Colors.white,
+//         borderRadius: const BorderRadius.only(
+//             topLeft: Radius.circular(30.0),
+//             topRight: Radius.circular(30.0),
+//             bottomLeft: Radius.circular(10.0),
+//             bottomRight: Radius.circular(10.0))),
+//     height: MediaQuery.of(context).size.height,
+//     width: MediaQuery.of(context).size.width,
+//     child: Padding(
+//       padding: const EdgeInsets.all(15.0),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         children: const [
+//           Align(
+//             alignment: Alignment.topLeft,
+//             child: Text(
+//               'Top Hospital',
+//               style: TextStyle(
+//                   fontSize: 18,
+//                   fontWeight: FontWeight.bold),
+//             ),
+//           ),
+//         ],
+//       ),
+//     ),
+//   ),
+// ),
